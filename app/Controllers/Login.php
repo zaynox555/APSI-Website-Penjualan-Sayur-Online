f5746e3240a0002e849643f9ca1144347a97c880
fahri-rahmanh <fahryrahman162@gmail.com>
2022-05-06 21:11:15 +0700

    disable redirect in login page

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -6,10 +6,10 @@ class Login extends BaseController
 {
     public function index()
     {
-        if ($_COOKIE['logged_in'] == 'true') {
-            return redirect()->to('/');
-        } else {
-            return view('login.php');
-        }
+        // if ($_COOKIE['logged_in'] == 'true') {
+        //     return redirect()->to('/');
+        // } else {
+        // }
+        return view('login.php');
     }
 }
